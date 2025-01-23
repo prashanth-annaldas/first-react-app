d51673e1d8e05730d3223b329d7a84e9250ed63c
prashanth <prashanthannaldas453@gmail.com>
2025-01-23 11:00:42 +0530

    this is second commit to the react project.

--- first-github-push/src/App.jsx
+++ first-github-push/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import HeadingComponent from './components/HeadingComponent'
 
 const App = () => {
   return (
     <div>
+      <HeadingComponent></HeadingComponent>
       Hello this is first github push react project 
     </div>
   )
---
+++ first-github-push/src/components/HeadingComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeadingComponent = () => {
+  return (
+    <h1>
+      Heading for this react Project.
+    </h1>
+  )
+}
+
+export default HeadingComponent
